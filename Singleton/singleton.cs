3f49edfea8e787a840c19d4eaf66c3c90b835aa5
ede02 <edennis0204@gmail.com>
2022-02-17 01:43:31 -0400

    Add project files.

---
+++ Singleton/singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class singleton
+    {
+        private static singleton instance = null;
+
+        public string colors = "";
+
+        protected singleton() {
+            colors = "Negro";
+        }
+
+        public  static singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new singleton();
+
+                return instance;
+            }
+        }
+        
+    }
+}
